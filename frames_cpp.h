f25eed865646858b6a19c42e7e9a3394baa2ac59
Aaron John Sabu <38135463+aaronjohnsabu1999@users.noreply.github.com>
2019-03-10 13:18:55 +0530

    Updated frames_cpp.h
    Added and modified several functions. One more is remaining, which will depend on the datetime class being used in the code.

--- frames_cpp.h
+++ frames_cpp.h
@@ -6,35 +6,6 @@
 
 using namespace std;
 
-float radians(float deg)
-{
-    float rad = deg*0.0174532925;
-    return rad;
-}
-
-int sgn(float num)
-{
-    if(num < 0)
-        return -1;
-    else if(num == 0)
-        return 0;
-    else
-        return 1;
-}
-
-float* ecif2ecef(float* v_x_i, time_t t)
-{
-    // Input ecif vector and time since epoch in seconds
-	// Output ecef vector
-    time_t ut_sec = (EPOCH - EQUINOX) + t; // universal time vector in sec
-	float theta = w_earth*ut_sec;
-	float m_DCM[3][3] = {{cos(theta), sin(theta), 0.0}, {-1*sin(theta), cos(theta),0.0}, {0.0,0.0,1.0}};
-	static float v_x_e[3];
-	for(int i = 0; i<3; i++)
-	    v_x_e[i] = dot(m_DCM[i],v_x_i);
-	return v_x_e;
-}
-
 float* latlon(float v_x[3])
 {
 	// get the latitude and longitude in degrees given position in ECEF
@@ -59,7 +30,32 @@ float* latlon(float v_x[3])
     return lattlonn;
 }
 
-float* ecef2ecif(float* v_x_e, time_t t)
+float* ecif2ecef(float* v_x_i, time_t t)
+{
+    // Input ecif vector and time since epoch in seconds
+	// Output ecef vector
+    time_t ut_sec = (EPOCH - EQUINOX) + t; // universal time vector in sec
+	float theta = w_earth*ut_sec;
+	float m_DCM[3][3] = {{cos(theta), sin(theta), 0.0}, {-1*sin(theta), cos(theta),0.0}, {0.0,0.0,1.0}};
+	static float v_x_e[3];
+	for(int i = 0; i<3; i++)
+	    v_x_e[i] = dot(m_DCM[i],v_x_i);
+	return v_x_e;
+}
+
+/*
+float** ecif2ecefR(float t):
+{
+    float ut_sec = (EPOCH - EQUINOX) + t;                               // universal time vector in sec
+	float st_sec = STEPRUT*ut_sec;                                      // sidereal time vector in sec
+	float phi = st_sec*W_EARTH;                                         // sidereal time vector in rad
+    float** TEI = {{cos(phi), sin(phi), 0.0}, {-sin(phi), cos(phi), 0.0}, {0.0, 0.0, 1.0}};
+
+	return TEI
+}
+*/
+
+float* ecef2ecif(float v_x_e[3], time_t t)
 {
     // Input ecef vector and time since epoch in seconds
 	// Output ecif vector
@@ -70,6 +66,97 @@ float* ecef2ecif(float* v_x_e, time_t t)
 	for(int i = 0; i<3; i++)
 	    v_x_i[i] = dot(DCM[i], v_x_e);
 	return v_x_i;
+}
+
+float* ecif2orbit(float v_pos_i[3], float v_vel_i[3], float v_x_i[3])
+{
+    // Input: v_pos_i is position in eci frame , v_vel_i is velocity in eci frame, v_x_i is vector to be transformed
+	// Output: vector components in orbit frame
+	float z[3], y[3], x[3];
+	for(int i = 0; i<3; i++)
+    {
+        z[i] = - (v_pos_i[i])/v_pos_i.norm();
+        y[i] = (cross(v_vel_i, v_pos_i))[i];
+    }
+    y = y/y.norm;
+	for(int i = 0; i<3; i++)
+        x[i] = (cross(y, z))[i];
+	x = x/x.norm;
+
+###	float m_DCM_OI[3][3] = {x, y, z};
+	float v_x_o[3];
+	for(int i = 0; i<3; i++)
+        v_x_o[i] = dot(m_DCM_OI[i], v_x_i);
+
+	return v_x_o;
+}
+
+quat qBI2qBO(quat v_q_BI, float v_pos_i[3], float v_vel_i[3])
+{
+    // input: unit quaternion which rotates ecif vector to body frame, position and velocity in ecif
+	// output: unit quaternion which rotates orbit frame vector to body frame
+	// Orbit frame def:	z_o = nadir(opposite to satellite position vector) y_o: cross(v,r) x_o: cross(y,z)
+
+	float z[3], y[3], x[3];
+	for(int i = 0; i<3; i++)
+    {
+        z[i] = - (v_pos_i[i])/v_pos_i.norm();
+        y[i] = (cross(v_vel_i, v_pos_i))[i];
+    }
+    y = y/y.norm;
+	for(int i = 0; i<3; i++)
+        x[i] = (cross(y, z))[i];
+	x = x/x.norm;
+
+	float temp[3][3] = {x, y, z};
+    matrix3 m_DCM_OI = matrix3(temp);
+
+	quat v_q_IO = rotm2quat(transpose(m_DCM_OI));
+	quat v_q_BO = quatMultiplyNorm(v_q_BI, v_q_IO);
+	if (v_q_BO[3] < 0.0)
+		v_q_BO = v_q_BO * (-1);
+	v_q_BO = v_q_BO/v_q_BO.norm();
+
+	return v_q_BO
+}
+
+quat qBO2qBI(quat v_q_BO, float v_pos_i[3], float v_vel_i[3]):
+{
+    // input: Unit quaternion which rotates orbit frame vector to body frame, position and velocity in ecif
+	// output: Unit quaternion which rotates ecif vector to body frame
+	// Orbit frame def:	z_o = nadir(opposite to satellite position vector) y_o: cross(v,r) x_o: cross(y,z)
+
+	float z[3], y[3], x[3];
+	for(int i = 0; i<3; i++)
+    {
+        z[i] = - (v_pos_i[i])/v_pos_i.norm();
+        y[i] = (cross(v_vel_i, v_pos_i))[i];
+    }
+    y = y/y.norm;
+	for(int i = 0; i<3; i++)
+        x[i] = (cross(y, z))[i];
+	x = x/x.norm;
+
+	float temp[3][3] = {x, y, z};
+    matrix3 m_DCM_OI = matrix3(temp);
+
+	v_q_OI = rotm2quat(m_DCM_OI);
+	v_q_BI = quatMultiplyNorm(v_q_BO, v_q_OI);
+	if (v_q_BI[3] < 0.0)
+		v_q_BI = v_q_BI * (-1);
+	v_qBI = v_q_BI/v_q_BI.norm();
+
+	return v_q_BI
+}
+
+float* wBIb2wBOb(float v_w_BI_b[3], quat v_q_BO, float v_w_IO_o[3])
+{
+// input: angular velocity of body wrt ecif in body frame, unit quaternion which rotates orbit vector to body frame and angular velocity of ecif wrt orbit frame in orbit frame
+// output: angular velocity of body frame wrt orbit frame in body frame
+    static float v[3];
+    for(int i = 0; i<3; i++)
+        v[i] = v_w_BI_b[i] - (quatRotate(v_q_BO, v_w_IO_o))[i];
+    return v;
 }
 
 float* ned2ecef(float* v, float lat, float lon)
@@ -94,16 +181,6 @@ float* ned2ecef(float* v, float lat, float lon)
     for(int i = 0; i<3; i++)
 	    y[i] = dot(m_DCM_n2e[i], v);
 	return y;
-}
-
-float* wBIb2wBOb(float v_w_BI_b[3], quat v_q_BO, float v_w_IO_o[3])
-{
-// input: angular velocity of body wrt ecif in body frame, unit quaternion which rotates orbit vector to body frame and angular velocity of ecif wrt orbit frame in orbit frame
-// output: angular velocity of body frame wrt orbit frame in body frame
-    static float v[3];
-    for(int i = 0; i<3; i++)
-        v[i] = v_w_BI_b[i] - (quatRotate(v_q_BO, v_w_IO_o))[i];
-    return v;
 }
 
 float* wBOb2wBIb(float v_w_BO_b[3], quat v_q_BO, float v_w_IO_o[3])
